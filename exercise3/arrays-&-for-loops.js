485f295bd50e11ccfefc84e1db0d49ce78a22ea7
Cecilia-Zheng <110209412+Cecilia-Zheng@users.noreply.github.com>
2022-09-15 22:23:07 +1000

    Add files via upload

---
+++ exercise3/arrays-&-for-loops.js
@@ -0,0 +1,26 @@
+var fruit1 = "banana";
+var fruit2 = "pineapple";
+var fruit3 = "apple";
+var fruit4 = "peach";
+var fruit5 = "orange";
+
+var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
+
+fruitArray[1] ="carrot";
+
+var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
+
+console.log(fruitArray);
+
+//*below: the what fruit doesn't have to be fruit, it can be named anything*/
+for(var fruit of fruitArray){
+    if(fruit === "carrot"){
+        console.log("this isn't a fruit")
+    } else {
+        console.log(fruit)
+    }
+}
+
+for(x of fruitArray){
+
+}
